db534af1b88278a0ac54d8f27c2460e3e02be97e
Philip Harrison <philhteoma@gmail.com>
2021-01-19 15:44:07 +0000

    minor fix

--- project_gavbot.py
+++ project_gavbot.py
@@ -48,7 +48,7 @@ def gavbot_index():
     if 'username' in session:
         try:
             # return render_template(dir_name+"templates/gavbot_index.html", gavbot=current_bots[session['username']], path=path)
-            render_template(dir_name+"templates/gavbot_index.html", gavbot=fetch_bot[session['username']], path=path)
+            render_template(dir_name+"templates/gavbot_index.html", gavbot=fetch_bot(session['username']), path=path)
         except KeyError:
             pass
     return render_template(dir_name+"templates/gavbot_index_null.html", path=path)
